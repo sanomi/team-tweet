1f95a1d4b10a46204f80fd4e6ec3cec1dfae5259
Sanomi <snrutledge@gmail.com>
2015-09-16 15:34:17 -0700

    close  #3 twitter works

---
+++ dist/bundle.js
@@ -0,0 +1,73 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+/******/
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ function(module, exports) {
+
+	"use strict";
+	
+	var hot = "HOT";
+	console.log("ES2015 is " + hot);
+	var authGuests = function authGuests() {
+	  if (localStorage["firebase:session::team-tweeet"]) {
+	    return;
+	  }
+	
+	  var ref = new Firebase("https://team-tweeet.firebaseio.com");
+	  ref.authWithOAuthPopup("twitter", function (error, authData) {
+	    if (error) {
+	      console.log("Login Failed!", error);
+	    } else {
+	      alert("Authenticated successfully with payload:", authData);
+	    }
+	  });
+	};
+	
+	authGuests();
+
+/***/ }
+/******/ ]);
+//# sourceMappingURL=bundle.js.map
--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
 let hot = "HOT";
 console.log(`ES2015 is ${hot}`);
+let authGuests = () => {
+if (localStorage["firebase:session::team-tweeet"]) {
+	return;
+}
+
+let ref = new Firebase("https://team-tweeet.firebaseio.com");
+ref.authWithOAuthPopup("twitter", function(error, authData) {
+  if (error) {
+    console.log("Login Failed!", error);
+  } else {
+    alert("Authenticated successfully with payload:", authData);
+  }
+});
+};
+
+authGuests();
